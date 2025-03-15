259ba6a536c96f2aac5f2ede0b1670dae02037bf
ncthangg <ncthang2206@gmail.com>
2025-03-15 20:55:42 +0700

    lab6

---
+++ Lab6/Menu/app/src/main/java/com/example/menu/CommentDialog.java
@@ -0,0 +1,9 @@
+package com.example.menu;
+
+import androidx.fragment.app.DialogFragment;
+
+public class CommentDialog {
+    public static DialogFragment newInstance() {
+        return null;
+    }
+}
---
+++ Lab6/Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CommentDialog DemoDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+    public void goodBye(View view){
+        switch ((view.getId())){
+            case R.id.btnAdd:
+                DialogFragment commentDialog= CommentDialog.newInstance();
+                commentDialog.show(getSupportFragmentManager(), "commentDialog");
+                break;
+
+        }
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Exit:
+                finishAffinity();
+                break;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+}
